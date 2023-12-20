febc018c2141becaddad5b7ea46c3fc9f01138de
Ameer <ameerapex496@gmail.com>
2023-12-20 19:11:11 +0100

    added great logic when buttons are clicked

--- src/script.js
+++ src/script.js
@@ -1,10 +1,21 @@
 // selecting elements
 var numberBtns = document.querySelectorAll(".number-Btn");
+var showResultBox = document.getElementsByClassName("show-Result-Box")[0];
 // gelobal varibales
 // functions
+var showData = function (valueOfBtn) {
+    if (showResultBox.textContent === "0") {
+        showResultBox.textContent = valueOfBtn;
+    }
+    else {
+        showResultBox.textContent += valueOfBtn;
+    }
+};
 // event lisnters
 numberBtns.forEach(function (numberBtn) {
     numberBtn.addEventListener("click", function () {
         console.log(numberBtn.value);
+        var valueOfBtn = numberBtn.value;
+        showData(valueOfBtn);
     });
 });
--- src/script.ts
+++ src/script.ts
@@ -1,13 +1,23 @@
 // selecting elements
 const numberBtns = document.querySelectorAll(".number-Btn");
+const showResultBox = document.getElementsByClassName("show-Result-Box")[0];
 
 // gelobal varibales
 
 // functions
+const showData = (valueOfBtn) => {
+  if (showResultBox.textContent === "0") {
+    showResultBox.textContent = valueOfBtn;
+  } else {
+    showResultBox.textContent += valueOfBtn;
+  }
+};
 
 // event lisnters
 numberBtns.forEach((numberBtn) => {
   numberBtn.addEventListener("click", () => {
     console.log(numberBtn.value);
+    let valueOfBtn = numberBtn.value;
+    showData(valueOfBtn);
   });
 });
