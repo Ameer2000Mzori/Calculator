0bfa1eb81b9c57830f48d1d6c33510ace51c3eba
Ameer <ameerapex496@gmail.com>
2023-12-20 19:18:37 +0100

    added secondNum func + oprator func and more

--- src/script.js
+++ src/script.js
@@ -1,8 +1,10 @@
 // selecting elements
 var numberBtns = document.querySelectorAll(".number-Btn");
 var showResultBox = document.getElementsByClassName("show-Result-Box")[0];
+var opretorBtns = document.querySelectorAll(".opretor-Btn");
 // gelobal varibales
 // functions
+// get the first number function
 var showData = function (valueOfBtn) {
     if (showResultBox.textContent === "0") {
         showResultBox.textContent = valueOfBtn;
@@ -11,11 +13,36 @@ var showData = function (valueOfBtn) {
         showResultBox.textContent += valueOfBtn;
     }
 };
+// start oprating functions
+var startOprating = function (opratorVal) {
+    var firstNum = showResultBox.textContent;
+    opratorVal = opratorVal;
+    console.log(opratorVal);
+};
+// second number function
+var secondNumFunc = function (secondNum, opratorVal, firstNum) {
+    secondNum = secondNum;
+    console.log("our first num: ", firstNum);
+    console.log("our oprator : ", opratorVal);
+    console.log("our second num: ", secondNum);
+};
 // event lisnters
 numberBtns.forEach(function (numberBtn) {
     numberBtn.addEventListener("click", function () {
-        console.log(numberBtn.value);
-        var valueOfBtn = numberBtn.value;
-        showData(valueOfBtn);
+        if (opratorVal && firstNum) {
+            var secondNum = numberBtn.value;
+            secondNumFunc(secondNum);
+        }
+        else {
+            console.log(numberBtn.value);
+            var valueOfBtn = numberBtn.value;
+            showData(valueOfBtn);
+        }
+    });
+});
+opretorBtns.forEach(function (opretorBtn) {
+    opretorBtn.addEventListener("click", function () {
+        var opratorVal = opretorBtn.value;
+        startOprating(opratorVal);
     });
 });
--- src/script.ts
+++ src/script.ts
@@ -1,10 +1,12 @@
 // selecting elements
 const numberBtns = document.querySelectorAll(".number-Btn");
 const showResultBox = document.getElementsByClassName("show-Result-Box")[0];
-
+const opretorBtns = document.querySelectorAll(".opretor-Btn");
 // gelobal varibales
 
 // functions
+
+// get the first number function
 const showData = (valueOfBtn) => {
   if (showResultBox.textContent === "0") {
     showResultBox.textContent = valueOfBtn;
@@ -13,11 +15,39 @@ const showData = (valueOfBtn) => {
   }
 };
 
+// start oprating functions
+const startOprating = (opratorVal) => {
+  let firstNum = showResultBox.textContent;
+  opratorVal = opratorVal;
+  console.log(opratorVal);
+};
+
+// second number function
+const secondNumFunc = (secondNum, opratorVal, firstNum) => {
+  secondNum = secondNum;
+  console.log(`our first num: `, firstNum);
+
+  console.log(`our oprator : `, opratorVal);
+  console.log(`our second num: `, secondNum);
+};
+
 // event lisnters
 numberBtns.forEach((numberBtn) => {
   numberBtn.addEventListener("click", () => {
-    console.log(numberBtn.value);
-    let valueOfBtn = numberBtn.value;
-    showData(valueOfBtn);
+    if (opratorVal && firstNum) {
+      let secondNum = numberBtn.value;
+      secondNumFunc(secondNum);
+    } else {
+      console.log(numberBtn.value);
+      let valueOfBtn = numberBtn.value;
+      showData(valueOfBtn);
+    }
+  });
+});
+
+opretorBtns.forEach((opretorBtn) => {
+  opretorBtn.addEventListener("click", () => {
+    let opratorVal = opretorBtn.value;
+    startOprating(opratorVal);
   });
 });
